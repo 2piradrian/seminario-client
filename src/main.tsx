11466951f1bf0a7ddf28c4d2e02d9b9888bc8c8e
adrian <rodriguezcadr@gmail.com>
2025-08-26 19:52:53 -0300

    presentation layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import RoutesManager from './ui/routes/routes-manager'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <RoutesManager />
+    <Toaster
+      position="bottom-left"
+      toastOptions={{duration: 7000}}
+    />
+  </>
 )
