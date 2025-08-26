cbe943d602c112b1633dedf661edc6725831918a
adrian <rodriguezcadr@gmail.com>
2025-08-26 20:04:12 -0300

    folder structure and repository provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import RoutesManager from './ui/routes/routes-manager'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { RepositoriesProvider } from './core/provider/RepositoryProvider'
 
 createRoot(document.getElementById('root')!).render(
-  <>
+  <RepositoriesProvider>
     <RoutesManager />
     <Toaster
       position="bottom-left"
       toastOptions={{duration: 7000}}
     />
-  </>
+  </RepositoriesProvider>
 )
