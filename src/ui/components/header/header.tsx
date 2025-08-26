02013460cfe7b57d16c7eb033c0454ab6cb555c8
adrian <rodriguezcadr@gmail.com>
2025-08-26 19:58:06 -0300

    layout added

---
+++ src/ui/components/header/header.tsx
@@ -0,0 +1,10 @@
+import style from './style.module.css';
+
+export default function Header() {
+  return (
+    <header className={`${style.container} container`}>
+      <div className={`${style.delimiter} delimiter`}>
+      </div>
+    </header>
+  );
+}
---
+++ src/ui/layout/layout.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/header";
+import "../styles/global.css";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+    return (
+        <div className="container">
+            <Header />
+            <main>{children}</main>
+        </div>
+    );
+}
