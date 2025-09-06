3bde2972267c2d9de084662884a90488849b6be7
anadruetta29 <anadruetta2023@gmail.com>
2025-09-06 20:58:30 -0300

    add: formlogin
    added two input labels on form login and small text

--- src/ui/components/molecules/login-form/login-form.tsx
+++ src/ui/components/molecules/login-form/login-form.tsx
@@ -1,5 +1,7 @@
 import MainButton from "../../atoms/main-button/main-button";
 import InputLabel from "../../atoms/input-label/input-label";
+import MediumTitle from "../../atoms/medium-title/medium-title";
+import SmallTitle from "../../atoms/small-title/small-title";
 import style from "./style.module.css";
 
 type Props = {
@@ -9,11 +11,17 @@ type Props = {
 export default function LoginForm({onSubmit}: Props) {
     return (
         <form onSubmit={onSubmit} className={style.container}>
+            <MediumTitle text="Iniciar sesión"/>
             <div className={style.inputDelimiter}>
-                <InputLabel id="email" placeholder="Email" required type="text" label="Ingrese su email"/>
+                <InputLabel id="email" placeholder="Email" required type="text" label="Ingrese su email:"/>
+            </div>
+            <div className={style.inputDelimiter}>
+                <InputLabel id="password" placeholder="Contraseña" required type="text" label="Ingrese su contraseña:"/>
             </div>
             <MainButton onClick={() => {}} text="Ingresar" type="submit"/>           
 
+            <SmallTitle text="Olvidaste tu contraseña?"/>
+            <SmallTitle text="No tenes una cuenta? Registrate"/>
         </form>
     )
     
