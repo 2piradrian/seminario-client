ec2edcc78553a424444320a7cc249dbf63a8d111
DisenRumble <juanpabloquintana2005@gmail.com>
2025-09-16 02:40:04 -0300

    test(login)

--- src/test/jest.setup.ts
+++ src/test/jest.setup.ts
@@ -1,1 +1,17 @@
 import "@testing-library/jest-dom"
+import "fast-text-encoding"
+
+if (!HTMLFormElement.prototype.requestSubmit) {
+    HTMLFormElement.prototype.requestSubmit = function(submitter) {
+        if (submitter) {
+            submitter.click()
+        } else{
+           const tmpSubmit = document.createElement('button');
+            tmpSubmit.type = 'submit';
+            tmpSubmit.style.display = 'none';
+            this.appendChild(tmpSubmit);
+            tmpSubmit.click();
+            this.removeChild(tmpSubmit); 
+        }
+    }
+}
