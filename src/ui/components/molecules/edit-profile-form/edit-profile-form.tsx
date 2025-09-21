a2956e9ed391f03509f76de56e2a50c7c4ec0a98
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-09-21 16:51:13 -0300

    refactor: update styles and structure in edit profile form and secondary button

--- src/ui/components/molecules/edit-profile-form/edit-profile-form.tsx
+++ src/ui/components/molecules/edit-profile-form/edit-profile-form.tsx
@@ -1,19 +1,26 @@
 import InputLabel from '../../atoms/input-label/input-label'
 import LargeTitle from '../../atoms/large-title/large-title'
+import MainButton from '../../atoms/main-button/main-button'
 import MediumTitle from '../../atoms/medium-title/medium-title'
 import MultipleSelector from '../../atoms/multiple-selector/multiple-selector'
+import SecondaryButton from '../../atoms/secondary-button/secondary-button'
 import TextAreaLabel from '../../atoms/textarea-label/textarea-label'
 import style from './style.module.css'
 
 export default function EditProfileForm() {
     return (
-        <div className={style.container}>
+        <form className={style.container}>
             <LargeTitle text="Editar perfil" />
             <div className={style.content}>
                 <MediumTitle text="Información personal" />
                 <div className={style.section}>
                     <InputLabel id="name" placeholder="Nombre" required type="text" label="Nombre" />
                     <InputLabel id="surname" placeholder="Apellido" required type="text" label="Apellido" />
+                </div>
+                <MediumTitle text="Imágenes" />
+                <div className={style.section}>
+                    <InputLabel id="profileImage" placeholder="URL de la imágen" required type="text" label="Imágen de perfil" />
+                    <InputLabel id="portraitImage" placeholder="URL de la imágen" required type="text" label="Imágen de portada" />
                 </div>
                 <MediumTitle text="Sobre mi" />
                 <div className={style.section}>
@@ -42,6 +49,8 @@ export default function EditProfileForm() {
                     />
                 </div>
             </div>
-        </div>
+            <MainButton enabled text="Guardar cambios" type="submit" onClick={() => { console.log("Guardar cambios") }} />
+            <SecondaryButton enabled text="Cancelar" type="button" onClick={() => { console.log("Cancelar") }} />
+        </form>
     )
 }
