ef338d1720a2338fd29980500445cb832605a0bb
milagrso <milagrosberoncapra1@gmail.com>
2025-09-22 18:05:20 -0300

    register connection

--- src/ui/routes/register/viewmodel.tsx
+++ src/ui/routes/register/viewmodel.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { Regex, Errors } from "../../../domain";
 import toast from "react-hot-toast";
+import { useRepositories } from "../../../core/provider/RepositoryProvider";
+import type { RegisterUserReq } from "../../../domain/dto/auth/request/RegisterUserReq";
 
 export function ViewModel() {
+
+    const { authRepository } = useRepositories();
 
     const [error, setError] = useState<string | null>(null); 
     
@@ -14,31 +18,47 @@ export function ViewModel() {
     }, [error]); 
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement> ) => {
-        e.preventDefault();
+        try {
+            e.preventDefault();
 
-        const form = Object.fromEntries(new FormData(e.currentTarget)) as { 
-            name?: string;
-            surname?: string; 
-            email?: string; 
-            password?: string 
-        };
+            const form = Object.fromEntries(new FormData(e.currentTarget)) as { 
+                name?: string;
+                surname?: string; 
+                email?: string; 
+                password?: string 
+            };
 
 
-        if(!Regex.NAME.test(form.name || "")){
-            return setError(Errors.INVALID_NAME);
+            if(!Regex.NAME.test(form.name || "")){
+                return setError(Errors.INVALID_NAME);
+            }
+
+            if(!Regex.SURNAME.test(form.surname || "")){
+                return setError(Errors.INVALID_LASTNAME);
+            }
+
+            if(!Regex.EMAIL.test(form.email || "")){
+                return setError(Errors.INVALID_EMAIL);
+            }
+            
+            if(!Regex.PASSWORD.test(form.password || "")){
+                return setError(Errors.INVALID_PASSWORD);
+            } 
+            const dto: RegisterUserReq = {
+                name: form.email!!,
+                surname: form.surname!!,
+                email: form.email!!,
+                password: form.password!!,
+            }
+
+            await authRepository.register(dto);
+
+            toast.success("Sesión iniciada correctamente")
         }
-
-        if(!Regex.SURNAME.test(form.surname || "")){
-            return setError(Errors.INVALID_LASTNAME);
+        catch (error) {
+            toast.error("Error al iniciar sesión")
         }
-
-        if(!Regex.EMAIL.test(form.email || "")){
-            return setError(Errors.INVALID_EMAIL);
-        }
-        
-        if(!Regex.PASSWORD.test(form.password || "")){
-            return setError(Errors.INVALID_PASSWORD);
-        }   
+  
     }
 
     return {
