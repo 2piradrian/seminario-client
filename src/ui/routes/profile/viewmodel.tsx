492b1287d1b1deaad89392a9968b7236c1891b1a
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-09-23 08:06:48 -0300

    feat(session): implement useSesion hook for session management

--- src/ui/routes/profile/viewmodel.tsx
+++ src/ui/routes/profile/viewmodel.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from "react-router-dom";
+import useSesion from "../../hooks/useSesion";
 
 export default function ViewModel() {
 
+    const { } = useSesion();
     const navigate = useNavigate();
+
     
     const goToEditProfile = () => {
         navigate("/profile/edit");
