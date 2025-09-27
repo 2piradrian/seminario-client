ba36e80b96c5ceecb46c5b3ec9a408fb6b4a0417
anadruetta29 <anadruetta2023@gmail.com>
2025-09-27 12:41:46 -0300

    add: single-input-image component
    add: single-input-image component

--- src/domain/validator/regex.ts
+++ src/domain/validator/regex.ts
@@ -8,8 +8,6 @@ export class Regex {
 
     public static readonly PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$/;
 
-    public static readonly IMAGE_URL = /^https:\/\/.+$/;
-
     public static readonly SHORT_DESCRIPTION = /^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?'"¡¿@%&$*+_:;\-\s]{1,50}$/;
 
     public static readonly LONG_DESCRIPTION  = /^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?'"¡¿@%&$*+_:;\-\s]{1,150}$/;
---
+++ src/ui/components/atoms/single-image-input/single-image-input.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import MainButton from "../main-button/main-button";
+import style from "./style.module.css";
+
+type Props = {
+    id: string;
+    label?: string;
+    value?: File | null;
+    required: boolean;
+    accept?: string;
+    buttonText?: string;
+    onChange?: (file: File | null) => void;
+}
+
+export default function SingleImageInput({
+    id,
+    label,
+    value = null,
+    required,
+    accept = "image/*",
+    buttonText,
+    onChange
+    }: Props) {
+
+    const [file, setFile] = useState<File | null>(value);
+
+    useEffect(() => {
+        setFile(value);
+    }, [value]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files || e.target.files.length === 0) return;
+
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+        onChange?.(selectedFile);
+    };
+
+    const handleClick = () => {
+        const input = document.getElementById(id) as HTMLInputElement | null;
+        input?.click();
+    };
+
+    return (
+        <div className={style.container}>
+            <label htmlFor={id}>{label}</label>
+            <input 
+                className={style.hiddenInput}
+                id={id}
+                type="file"
+                required={required}
+                accept={accept}
+                onChange={handleChange}
+            />
+            <MainButton
+                text={buttonText || "Seleccionar archivo"}
+                type="button"
+                enabled={true}
+                onClick={handleClick}
+            />
+            <div className={style.fileName}>
+                <strong>Imágen seleccionada:</strong>{" "}
+                {file ? file.name : "Mantener imágen actual"}
+            </div>
+        </div>
+    );
+}
