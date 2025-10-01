c49192ffe437983b0e396af6a29368b90c636567
JulianTs0 <juliantorresssr@gmail.com>
2025-10-01 19:59:37 -0300

    fix: Edit post request

--- src/domain/dto/post/request/EditPostReq.ts
+++ src/domain/dto/post/request/EditPostReq.ts
@@ -1,8 +1,12 @@
+import type { Category } from "../../../entity/category";
+import type { Post } from "../../../entity/post";
 import type { Sesion } from "../../../entity/sesion";
 
 export interface EditPostReq {
     sesion: Sesion;
+    post: Post;
     title: string;
     content: string;
+    category: Category;
     image: string; 
 }
