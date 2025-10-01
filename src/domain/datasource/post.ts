a57f7d2bbc5bd79d05ca9c455db0ae1d951f095a
JulianTs0 <juliantorresssr@gmail.com>
2025-10-01 20:19:47 -0300

    add: new abstracts methods to datasource post

--- src/domain/datasource/post.ts
+++ src/domain/datasource/post.ts
@@ -5,10 +5,15 @@ import type { CreatePostRes } from '../dto/post/response/CreatePostRes';
 import type { EditPostReq } from '../dto/post/request/EditPostReq';
 import type { EditPostRes } from '../dto/post/response/EditPostRes';
 import type { DeletePostReq } from '../dto/post/request/DeletePostReq';
+import type { GetPostPageReq } from '../dto/post/request/GetPostPageReq';
+import type { GetPostPageRes } from '../dto/post/response/GetPostPageRes';
+import type { TogglePostVotesReq } from '../dto/post/request/TogglePostVotesReq';
 
 export abstract class PostDatasourceI {
-    abstract getById(dto: GetPostByIdReq): Promise<GetPostByIdRes>;    
+    abstract getById(dto: GetPostByIdReq): Promise<GetPostByIdRes>;
+    abstract getPostPage(dto: GetPostPageReq): Promise<GetPostPageRes>;
     abstract create(dto: CreatePostReq): Promise<CreatePostRes>;
     abstract edit(dto: EditPostReq): Promise<EditPostRes>;
     abstract delete(dto: DeletePostReq): Promise<void>;
+    abstract togleVotes(dto: TogglePostVotesReq): Promise<void>;
 }
