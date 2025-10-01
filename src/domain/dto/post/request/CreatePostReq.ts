3f0550a1180895c7e652d6d473b811b3cbb30989
JulianTs0 <juliantorresssr@gmail.com>
2025-10-01 19:55:47 -0300

    fix: Create post request

--- src/domain/dto/post/request/CreatePostReq.ts
+++ src/domain/dto/post/request/CreatePostReq.ts
@@ -1,8 +1,12 @@
+import type { Category } from "../../../entity/category";
+import type { Page } from "../../../entity/page";
 import { Sesion } from "../../../entity/sesion";
 
 export interface CreatePostReq {
     sesion: Sesion;
     title: string;
     content: string;
+    page: Page;
+    category: Category;
     image: string;   
 }
