8976bbc197c40a9340b0c1a51ba3add281a8f4ca
JulianTs0 <juliantorresssr@gmail.com>
2025-10-02 10:18:02 -0300

    add: page api datasource

---
+++ src/infrastructure/datasource/page-api.ts
@@ -0,0 +1,85 @@
+import { HTTPClient } from "../../core";
+import { type CreatePageReq, type EditPageReq, type DeletePageReq, type GetPageByIdReq, type GetPageByUserIdReq, type GetPageByIdRes, type GetPageByUserIdRes, ErrorHandler, type Error, type PageDatasourceI } from "../../domain";
+
+export class PageApiDataSource implements PageDatasourceI { 
+
+    private httpClient: HTTPClient;
+
+    constructor(){
+        this.httpClient = new HTTPClient();
+    }
+
+    public async getById(dto: GetPageByIdReq): Promise<GetPageByIdRes> {
+        try {
+            const response = await this.httpClient.get("/pages/get-by-id", dto.pageId);
+            
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            }
+            
+            return response;
+        } catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+
+    public async getByUserId(dto: GetPageByUserIdReq): Promise<GetPageByUserIdRes> {
+        try {
+            const response = await this.httpClient.get("/pages/get-by-user-id", dto.userId);
+            
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            
+            }
+
+            return response;
+        } catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+
+    public async create(dto: CreatePageReq): Promise<void> {
+        try {
+            const response = await this.httpClient.post("/pages/create", { ...dto }, dto.sesion.getAccessToken());
+            
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            
+            }
+
+            return response;
+        } catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+
+    public async edit(dto: EditPageReq): Promise<void> {
+        try {
+            const response = await this.httpClient.put("/pages/edit", { ...dto }, dto.sesion.getAccessToken());
+            
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            
+            }
+
+            return response;
+        } catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    } 
+
+    public async delete(dto: DeletePageReq): Promise<void> {
+        try {
+            const response = await this.httpClient.delete("/pages/delete", { ...dto }, dto.sesion.getAccessToken());
+            
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            
+            }
+
+            return response;
+        } catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+}
