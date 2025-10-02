4e486eb0b47f5b76cf25249bbaf731192f26a5f0
JulianTs0 <juliantorresssr@gmail.com>
2025-10-02 09:11:40 -0300

    fix: getPostById response dto

--- src/domain/dto/post/response/GetPostByIdRes.ts
+++ src/domain/dto/post/response/GetPostByIdRes.ts
@@ -1,13 +1,10 @@
 import type { Category } from "../../../entity/category";
-import type { Page } from "../../../entity/page";
-import type { Post } from "../../../entity/post";
-import type { User } from "../../../entity/user";
 
 export interface GetPostByIdRes {
-    author: User;
-    post: Post;
-    page: Page;
-    image: string;
+    authorId: string;
+    postId: string;
+    pageId: string;
+    imageId: string;
     title: string;
     content: string;
     views: number;
