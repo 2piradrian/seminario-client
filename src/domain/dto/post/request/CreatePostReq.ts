382c7328f7fd2145a6762902f1c05823bc2e59cf
JulianTs0 <juliantorresssr@gmail.com>
2025-10-02 09:06:30 -0300

    fix: CreatePost request id

--- src/domain/dto/post/request/CreatePostReq.ts
+++ src/domain/dto/post/request/CreatePostReq.ts
@@ -1,12 +1,11 @@
 import type { Category } from "../../../entity/category";
-import type { Page } from "../../../entity/page";
 import { Sesion } from "../../../entity/sesion";
 
 export interface CreatePostReq {
     sesion: Sesion;
     title: string;
     content: string;
-    page: Page;
+    pageId: string;
     category: Category;
     image: string;   
 }
