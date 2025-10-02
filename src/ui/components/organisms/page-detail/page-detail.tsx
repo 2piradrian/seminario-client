7ac79255ce7a00063ad16111542f6387993b6e4a
milagrso <milagrosberoncapra1@gmail.com>
2025-10-02 18:16:44 -0300

    add: page datail

---
+++ src/ui/components/organisms/page-detail/page-detail.tsx
@@ -0,0 +1,31 @@
+import type { Page } from "../../../../domain";
+import MediumTitle from "../../atoms/medium-title/medium-title";
+import style from "./style.module.css"
+
+type Props = {
+    page: Page
+}
+
+export default function PageDetail({ page }: Props) {
+
+    return(
+        <div className={style.container}>
+            <div className={style.detail}>
+                <MediumTitle text="Detalles"/>
+                <p className={style.text}>{page.longDescription}</p>
+            </div>
+            <div className={style.members}>
+                <MediumTitle text="Miembros" />
+                {page.members.length === 0 ? (
+                    <p className={style.text}>No hay miembros</p>
+                ): (
+                    <p className={style.text}>
+                        {page.members.length} {page.members.length === 1 ? "miembro" : "miembros"}
+                    </p>
+                )}
+
+            </div>
+        </div>
+    )
+    
+}
