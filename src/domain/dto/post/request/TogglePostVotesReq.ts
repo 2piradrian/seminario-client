a565538ff98432259a4ed65c8ef0568b4541f570
JulianTs0 <juliantorresssr@gmail.com>
2025-10-02 09:08:45 -0300

    fix: toggleVote request id

--- src/domain/dto/post/request/TogglePostVotesReq.ts
+++ src/domain/dto/post/request/TogglePostVotesReq.ts
@@ -1,9 +1,8 @@
-import type { Post } from "../../../entity/post";
 import type { Sesion } from "../../../entity/sesion";
 import type { Vote } from "../../../entity/vote";
 
 export interface TogglePostVotesReq {
     sesion: Sesion;
     voteType: Vote;
-    post: Post;
+    postId: string;
 }
