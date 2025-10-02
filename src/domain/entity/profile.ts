3f92a5ca22fc440309823a7d8a54f01ac379888f
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-10-02 18:21:30 -0300

    displayName

--- src/domain/entity/profile.ts
+++ src/domain/entity/profile.ts
@@ -4,7 +4,7 @@ import { UserProfile } from "./user-profile";
 export class Profile {
   private constructor(
     public id: string,
-    public name: string,
+    public displayName: string,
     public portraitImage: string,
     public profileImage: string,
     public shortDescription: string,
@@ -12,13 +12,13 @@ export class Profile {
   ) {}
 
   public static fromEntity(profile: UserProfile | Page): Profile {
-    const name = "surname" in profile
+    const displayName = "surname" in profile
       ? this.buildName(profile.name, profile.surname)
       : profile.name;
 
     return new Profile(
       profile.id,
-      name,
+      displayName,
       profile.portraitImage,
       profile.profileImage,
       profile.shortDescription,
