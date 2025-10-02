23b643a95bce69077553c9eb32aae9eb14393b41
JulianTs0 <juliantorresssr@gmail.com>
2025-10-02 09:41:53 -0300

    add: comment-api datasource

---
+++ src/infrastructure/datasource/comment-api.ts
@@ -0,0 +1,71 @@
+import { HTTPClient } from "../../core";
+import { type CreateCommentReq, type CreateCommentRes, type DeleteCommentReq, type GetCommentPageReq, type GetCommentPageRes, type ToggleCommentVotesReq, ErrorHandler, type Error, type CommentDatasourceI } from "../../domain";
+
+export class CommentApiDataSource implements CommentDatasourceI { 
+
+    private httpClient: HTTPClient;
+
+    constructor(){
+        this.httpClient = new HTTPClient();
+    }
+
+    public async getCommentPage(dto: GetCommentPageReq): Promise<GetCommentPageRes> {
+        try {
+            const response = await this.httpClient.get("/comments/get-comments", { ...dto });
+
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            }
+
+            return response;
+        } 
+        catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+
+    public async create(dto: CreateCommentReq): Promise<CreateCommentRes> {
+        try {
+            const response = await this.httpClient.post("/comments/create", { ...dto }, dto.sesion.getAccessToken());
+
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            }
+
+            return response; 
+        } 
+        catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+
+    public async delete(dto: DeleteCommentReq): Promise<void> {
+        try {
+            const response = await this.httpClient.delete("/comments/delete", { ...dto }, dto.sesion.getAccessToken());
+
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            }
+
+            return response;
+        }
+        catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+
+    public async toggleVotes(dto: ToggleCommentVotesReq): Promise<void> {
+        try {
+            const response = await this.httpClient.put("/comments/toggle-votes", { ...dto }, dto.sesion.getAccessToken());
+
+            if (response.error) {
+                throw ErrorHandler.handleError(response.error);
+            }
+
+            return response;
+        }
+        catch (error) {
+            throw ErrorHandler.handleError(error as Error);
+        }
+    }
+}
