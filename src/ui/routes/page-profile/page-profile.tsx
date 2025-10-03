a2c84acfd608dae0218f938418a5da547410d2dc
milagrso <milagrosberoncapra1@gmail.com>
2025-10-02 22:52:03 -0300

    fix: profile to page

--- src/ui/routes/page-profile/page-profile.tsx
+++ src/ui/routes/page-profile/page-profile.tsx
@@ -1,4 +1,6 @@
-import PostDetail from "../../components/organisms/post-detail/post-detail";
+import { Profile } from "../../../domain";
+import PageDetail from "../../components/organisms/page-detail/page-detail";
+import PostsList from "../../components/organisms/posts-list/posts-list";
 import ProfileHeader from "../../components/organisms/profile-header/profile-header";
 import Layout from "../../layout/layout";
 import ViewModel from "./viewmodel";
@@ -7,7 +9,7 @@ import ViewModel from "./viewmodel";
 export default function PageProfileRoute() {
     
     const { 
-        profile, 
+        page, 
         toggleFollow,
         isFollowing,
         onClickOnAvatar, 
@@ -15,36 +17,32 @@ export default function PageProfileRoute() {
         onDownVote,
         onUpVote,
         posts,
-        comments, 
     } = ViewModel();
 
     return(
      <Layout withHeader={true}>
-            { profile &&
+            { page &&
                 <>
                     <ProfileHeader 
                         isFollowing={isFollowing}
                         onClick={toggleFollow}
-                        profile={profile}
+                        profile={Profile.fromEntity(page)}
                         ownProfile={false}
                     />
+                    <PageDetail 
+                        page={page}
+                    />
                     {posts.length > 0 && (
-                        <PostDetail
-                            post={posts[0]}
-                            onClickOnAvatarPost={onClickOnAvatar}
-                            onClickOnComment={onClickOnComments}
-                            onDownVotePost={onDownVote}
-                            onUpVotePost={onUpVote}
-                            comments={comments}
-                            onClickOnAvatarComment={onClickOnAvatar}
-                            onDownVoteComment={onDownVote}
-                            onUpVoteComment={onUpVote}
+                        <PostsList
+                            posts={posts}
+                            onUpVote={onUpVote}
+                            onDownVote={onDownVote}
+                            onClickOnComments={onClickOnComments}
+                            onClickOnAvatar={onClickOnAvatar}
                         />
                     )}
-
                 </>  
             }
         </Layout>
     )
-
 }
