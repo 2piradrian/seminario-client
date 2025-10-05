ad175d4d8abe17f64bd883b09f61d6b1615391cd
JulianTs0 <juliantorresssr@gmail.com>
2025-10-05 11:48:53 -0300

    add: delete function to post-detail viewmodel

--- src/infrastructure/datasource/post-api.ts
+++ src/infrastructure/datasource/post-api.ts
@@ -70,6 +70,7 @@ export class PostApiDataSource implements PostDatasourceI {
 
     public async delete(dto: DeletePostReq): Promise<void> {
         try {
+            console.log(dto);
             const response = await this.httpClient.delete("/posts/delete", { ... dto}, dto.sesion.getAccessToken());
 
             if (response.error){
