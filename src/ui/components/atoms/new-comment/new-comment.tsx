2e79d5fd31e188d84f90146ca368daa255147012
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-10-05 18:19:05 -0300

    fix: conflicts

--- src/ui/components/atoms/new-comment/new-comment.tsx
+++ src/ui/components/atoms/new-comment/new-comment.tsx
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-import CommentButton from "../comments-button/comments-button"; 
-import style from "./style.module.css"; 
-
-
-type Props = { 
-    content: string; 
-    onChangeContent: (text: string) => void; 
-    onAddComment: () => void;
-}
-
-export default function NewComment({ content, onChangeContent, onAddComment }: Props) { 
-    return ( 
-        <div className={style.container}> 
-            <input 
-                type="text" 
-                value={content} 
-                onChange={(e) => onChangeContent(e.target.value)} 
-                placeholder="Agregar comentario..." 
-            /> 
-            <CommentButton 
-                onClick={onAddComment} 
-                text="Comentar" 
-            /> 
-        </div> 
-    ); 
-=======
 import CommentButton from "../comments-button/comments-button";
 import style from "./style.module.css";
 
@@ -47,5 +20,4 @@ export default function NewComment({ content, onChangeContent, onAddComment }: P
             <CommentButton onClick={onAddComment} text="Comentar" />
         </div>
     );
->>>>>>> Stashed changes
 }
