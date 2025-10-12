753775edf8609e817b86c158cbdd87226917c972
milagrso <milagrosberoncapra1@gmail.com>
2025-10-12 09:48:38 -0300

    add: search box

---
+++ src/ui/components/atoms/search-box/search-box.tsx
@@ -0,0 +1,33 @@
+import InputLabel from "../input-label/input-label";
+import MainIconButton from "../main-icon-button/main-icon-button";
+import style from "./style.module.css"
+import searchIcon from "../../../assets/icons/search.svg"
+
+type Props = {
+    onSearch: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export default function SearchBox({onSearch}:Props){
+    return(
+        <form className={style.container} onSubmit={onSearch}>
+            <InputLabel 
+                id="content"
+                placeholder="Buscar..."
+                type="text"
+                required
+            />
+            <div className={style.buttonContainer}>
+                <MainIconButton
+                    text=""
+                    type="submit"
+                    enabled={true}
+                    onClick={() => {}}
+                    icon={searchIcon}
+                    modifier={style.searchButton}
+                />
+            </div>
+
+        </form>
+
+    )
+}
