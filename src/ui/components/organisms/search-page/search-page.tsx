a866a27df97c66e6f44f49911136f5a5e15ed5c2
milagrso <milagrosberoncapra1@gmail.com>
2025-10-18 03:11:16 -0300

    fix: search page

--- src/ui/components/organisms/search-page/search-page.tsx
+++ src/ui/components/organisms/search-page/search-page.tsx
@@ -1,23 +1,72 @@
 import MediumTitle from "../../atoms/medium-title/medium-title";
 import SearchBox from "../../atoms/search-box/search-box";
-import SelectLabel from "../../atoms/select-label/select-label";
+import StateFullSelector from "../../atoms/state-full-selector/state-full-selector";
 import style from "./style.module.css";
 
 type Props = {
-    types: string[];
-    categories: string[];
+    contentTypes: string[];
+    styles: string[];
+    instruments: string[];
+    selectedType: string | null;
+    selectedStyle: string | null;
+    selectedInstrument: string | null;
+    onTypeChange: (value: string) => void;
+    onStyleChange: (value: string) => void;
+    onInstrumentChange: (value: string) => void;
+    showExtraFilters: boolean;
+    searchText: string;
+    onSearchChange: (value: string) => void;
 }
 
-export function SearchPage({categories, types}: Props) {
+export function SearchPage({
+    contentTypes, 
+    styles, 
+    instruments,
+    selectedType,
+    selectedStyle,
+    selectedInstrument,
+    onTypeChange,
+    onStyleChange,
+    onInstrumentChange,
+    showExtraFilters,
+    searchText,
+    onSearchChange
+}: Props) {
     return (
         <div className={style.container}>
             <div className={style.searchBox}>
-                <SearchBox onSearch={(e) => {}} />
+                <SearchBox onSearch={onSearchChange} />
             </div>
             <div className={style.filters}>
                 <MediumTitle text="Filtros" />
-                <SelectLabel id="search" label="Tipos" value={""} values={["Seleccionar", ...types]} />
-                <SelectLabel id="categories" label="Categorias" value="" values={["Seleccionar", ...categories]} />
+                <StateFullSelector 
+                    id="search" 
+                    label="Tipo" 
+                    value={selectedType || "Seleccionar"} 
+                    values={["Seleccionar", ...contentTypes]} 
+                    onChange={onTypeChange}
+                />
+                { showExtraFilters && (
+                    <>
+                        <StateFullSelector 
+                            id="Estilos" 
+                            label="Estilos" 
+                            value={selectedStyle|| "Seleccionar"}
+                            values={["Seleccionar", ...styles]}
+                            onChange={onStyleChange}
+                            disabled={!selectedType || selectedType === "Seleccionar"}
+                        />
+                        <StateFullSelector 
+                            id="Intrumentos" 
+                            label="Instrumentos" 
+                            value={selectedInstrument|| "Seleccionar"}
+                            values={["Seleccionar", ...instruments]} 
+                            onChange={onInstrumentChange}
+                            disabled={!selectedType || selectedType === "Seleccionar"}
+                        />
+                    </>
+                )}
+
             </div>
         </div>
     )
