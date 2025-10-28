4f08c0de30a0b9c2701415ca1be41f7b7b831e07
milagrso <milagrosberoncapra1@gmail.com>
2025-10-28 20:36:00 -0300

    add: admin route

--- src/ui/routes/routes-manager.tsx
+++ src/ui/routes/routes-manager.tsx
@@ -15,6 +15,7 @@ import SearchRoute from "./search/search";
 import FollowsRoute from "./follows/follows";
 import MainRoute from "./main/main";
 import NewEventRoute from "./new-event/new-event";
+import AdminRoute from "./admin/admin";
 
 export default function RoutesManager() {
     return(
@@ -39,6 +40,9 @@ export default function RoutesManager() {
                 {/* Page routes */}
                 <Route path="/new-page" element={<NewPageRoute />} />
                 <Route path="/page/:id" element={<PageProfileRoute />} />
+
+                {/* Admin route */}
+                <Route path="/admin" element={<AdminRoute/>} />
 
                 {/* Event routes*/}
                 <Route path="/new-event" element={<NewEventRoute />} /> 
