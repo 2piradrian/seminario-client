3a1cee165ff82eae789ea902f9dd639cac8b10fe
DisenRumble <juanpabloquintana2005@gmail.com>
2025-10-31 15:21:45 -0300

    add event list

---
+++ src/ui/components/organisms/event-list/event-list.tsx
@@ -0,0 +1,34 @@
+import type { Event } from "../../../../domain";
+import EventItem from "../../molecules/event-item/event-item";
+import style from "./style.module.css";
+
+type Props = {
+  events: Event[];
+  isMine?: boolean;
+  onClickOnEvent: (eventId: string) => void;
+  onClickOnAvatar: (event: Event) => void;
+  onClickDelete?: (eventId: string) => void;
+};
+
+export default function EventList({
+  events,
+  isMine = false,
+  onClickOnEvent,
+  onClickOnAvatar,
+  onClickDelete,
+}: Props) {
+  return (
+    <section className={style.list}>
+      {events.map((event) => (
+        <EventItem
+          key={event.id}
+          event={event}
+          onClickOnEvent={() => onClickOnEvent(event.id)}
+          onClickOnAvatar={() => onClickOnAvatar(event)}
+          onClickDelete={() => onClickDelete?.(event.id)}
+          isMine={isMine}
+        />
+      ))}
+    </section>
+  );
+}
