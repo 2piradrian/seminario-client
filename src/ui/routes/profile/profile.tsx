2aaa43e20625372d83b3c4112366939255ee923c
DisenRumble <juanpabloquintana2005@gmail.com>
2025-11-03 15:42:41 -0300

    fix

--- src/ui/routes/profile/profile.tsx
+++ src/ui/routes/profile/profile.tsx
@@ -15,6 +15,7 @@ export default function ProfileRoute(){
         posts,
         onClickOnPost,
         isMine,
+        events,
         cancelDelete,
         proceedDelete,
         isDeleteOpen,
