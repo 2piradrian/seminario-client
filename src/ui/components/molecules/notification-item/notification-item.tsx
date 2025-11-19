50edd9d9e86e9c4f39378ec85137e7386d2cc800
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-11-19 10:54:04 -0300

    Update notification-item.tsx

--- src/ui/components/molecules/notification-item/notification-item.tsx
+++ src/ui/components/molecules/notification-item/notification-item.tsx
@@ -1,15 +1,16 @@
+import type { Notification } from "../../../../domain";
 import style from "./style.module.css";
 
 type Props = {
     content,
-    notification
+    notification: Notification
 }
 
-export default function NotificationItem({ content }: Props) {
+export default function NotificationItem({ notification }: Props) {
     return(
         <div className={style.container}>
             <span className={style.notificationContent}>
-                {content}
+                {notification.buildMessage()}
             </span>
         </div>
     )
