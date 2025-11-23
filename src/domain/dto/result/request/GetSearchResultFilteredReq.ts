84cf53c072ced657da13910bb18847bc48a691be
milagrso <milagrosberoncapra1@gmail.com>
2025-11-22 21:55:12 -0300

    search

--- src/domain/dto/result/request/GetSearchResultFilteredReq.ts
+++ src/domain/dto/result/request/GetSearchResultFilteredReq.ts
@@ -1,13 +1,11 @@
-import type { Instrument } from "../../../entity/instrument";
-import type { Style } from "../../../entity/style";
 import type { Session } from "../../../entity/session.ts";
 
 export interface GetSearchResultFilteredReq {
     page: number;
     size: number;
     text: string;
-    styles: Style[];
-    instruments: Instrument[];
+    styles: string[];
+    instruments: string[];
     pageTypeId: string;
     postTypeId: string;
     contentTypeId: string;
