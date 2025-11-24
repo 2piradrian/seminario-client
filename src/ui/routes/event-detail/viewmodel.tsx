2f9094db3b2e2c826876161ec27ea2bce4263308
anadruetta29 <anadruetta2023@gmail.com>
2025-11-24 19:08:11 -0300

    fix: assisting button
    fix: assisting button

--- src/ui/routes/event-detail/viewmodel.tsx
+++ src/ui/routes/event-detail/viewmodel.tsx
@@ -46,6 +46,8 @@ export default function ViewModel() {
             
             setEvent(Event.fromObject(eventRes));
 
+            setIsAssisting(eventRes.isAssisting ?? false);
+
             await fetchProfiles().then();
         } 
         catch (error) {
