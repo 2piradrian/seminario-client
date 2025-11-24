89840a007b198f1713c6b279b9e47ae7c6db053f
anadruetta29 <anadruetta2023@gmail.com>
2025-11-24 16:28:50 -0300

    fix: page profile
    fix: page profile

--- src/domain/dto/page/request/CreatePageReq.ts
+++ src/domain/dto/page/request/CreatePageReq.ts
@@ -1,8 +1,7 @@
-import type { PageType } from "../../../entity/page-type";
 import type { Session } from "../../../entity/session.ts";
 
 export interface CreatePageReq {
     session: Session;
     name: string;
-    pageType: PageType;
+    pageTypeId: string;
 }
