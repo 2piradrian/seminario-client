a04a4eb216ce9191d9d7c2e0719079897548873d
JulianTs0 <juliantorresssr@gmail.com>
2025-12-02 15:38:56 -0300

    fix:  assistQuantity

--- src/domain/dto/event/response/GetEventByIdRes.ts
+++ src/domain/dto/event/response/GetEventByIdRes.ts
@@ -14,6 +14,6 @@ export interface GetEventByIdRes {
     dateEnd: Date;
     createdAt: Date;
     isAssisting: boolean;
-    assists: number;
+    assistsQuantity: number;
     status: Status;
 }
--- src/domain/dto/event/response/ToggleAssistRes.ts
+++ src/domain/dto/event/response/ToggleAssistRes.ts
@@ -14,6 +14,6 @@ export interface ToggleAssistRes {
     dateEnd: Date;
     createdAt: Date;
     isAssisting: boolean;
-    assists: number;
+    assistsQuantity: number;
     status: Status;
 }
