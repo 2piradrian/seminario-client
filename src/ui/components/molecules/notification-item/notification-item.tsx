06abfd72320c51877f6762bd526bfa802e7f4474
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-12-05 15:42:04 -0300

    notification

--- src/ui/components/molecules/notification-item/notification-item.tsx
+++ src/ui/components/molecules/notification-item/notification-item.tsx
@@ -11,14 +11,16 @@ type Props = {
 export default function NotificationItem({ notification, redirectToNotification }: Props) {
     return(
         <div className={`${style.container} ${style[notification.content.toString()]}`} onClick={() => redirectToNotification(notification)}>
-            <Avatar profile={notification.carriedOutBy.toProfile()} hideName />
-            <div className={style.notificationInfo}>
-                <span className={style.username}>
-                    {notification.carriedOutBy.toProfile().displayName}
-                </span>
-                <span className={style.notificationContent}>
-                    {notification.buildMessage()}
-                </span>
+            <div className={style.notification}>
+                <Avatar profile={notification.carriedOutBy.toProfile()} hideName />
+                <div className={style.notificationInfo}>
+                    <span className={style.username}>
+                        {notification.carriedOutBy.toProfile().displayName}
+                    </span>
+                    <span className={style.notificationContent}>
+                        {notification.buildMessage()}
+                    </span>
+                </div>
             </div>
         </div>
     )
