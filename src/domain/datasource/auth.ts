e340f7032715a0e25804159078e6ff78c073a420
JulianTs0 <juliantorresssr@gmail.com>
2025-12-08 09:24:54 -0300

    add: changePassword & recover repositories

--- src/domain/datasource/auth.ts
+++ src/domain/datasource/auth.ts
@@ -5,6 +5,8 @@ import type { AuthUserRes } from "../dto/auth/response/AuthUserRes";
 import type { LoginUserRes } from "../dto/auth/response/LoginUserRes";
 import type { GrantRoleUserReq } from "../dto/auth/request/GrantRoleUserReq";
 import type { RevokeRoleUserReq } from "../dto/auth/request/RevokeRoleUserReq";
+import type { ChangePasswordReq } from "../dto/auth/request/ChangePasswordReq";
+import type { RecoverPasswordReq } from "../dto/auth/request/RecoverPasswordReq";
 
 export abstract class AuthDataSourceI {
     abstract auth(dto: AuthUserReq): Promise<AuthUserRes>;
@@ -12,4 +14,6 @@ export abstract class AuthDataSourceI {
     abstract register(dto: RegisterUserReq): Promise<void>;
     abstract grantRole(dto: GrantRoleUserReq): Promise<void>;
     abstract revokeRole(dto: RevokeRoleUserReq): Promise<void>;
+    abstract changePassword(dto: ChangePasswordReq): Promise<void>;
+    abstract recoverPassword(dto: RecoverPasswordReq): Promise<void>;
 }
