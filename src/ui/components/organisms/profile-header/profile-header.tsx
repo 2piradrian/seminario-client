822dea2ddb9642947118b78b05777d35ad332a7f
JulianTs0 <juliantorresssr@gmail.com>
2025-12-18 12:28:16 -0300

    add: leave page and delete page functionality to profile header

--- src/ui/components/organisms/profile-header/profile-header.tsx
+++ src/ui/components/organisms/profile-header/profile-header.tsx
@@ -12,6 +12,8 @@ import { Profile } from "../../../../domain"
 import FollowCounter from "../../atoms/follow-counters/follow-counters"
 import SecondaryButton from "../../atoms/secondary-button/secondary-button"
 import style from "./style.module.css"
+import DestructiveButton from "../../atoms/destructive-button/destructive-button"
+import Modal from "../../molecules/modal/modal"
 
 type Props = {
     profile: Profile;
@@ -24,10 +26,19 @@ type Props = {
     onClickOnEditProfile: () => void;
     onClickOnCreatePost?: () => void;
     onClickOnCreatePage?: () => void;
+    onClickOnDeletePage?: () => void;
+    onCLickOnLeavePage?: () => void;
+    cancelLeave?: () => void,
+    cancelDeletePage?: () => void,
+    proceedLeave?: () => void,
+    proceedDeletePage?: () => void,
+    isLeaveOpen?: boolean,
+    isDeletePageOpen?: boolean,
     onClick?: () => void;
     onClickOnCalendar: () => void;
     onClickOnChat?: () => void;
     isPage?: boolean;
+    isMember?: boolean;
 };
 
 export default function ProfileHeader({
@@ -40,10 +51,19 @@ export default function ProfileHeader({
     onFollowingClick,
     onClickOnEditProfile,
     onClickOnCreatePage,
+    onClickOnDeletePage,
+    onCLickOnLeavePage,
     onClick,
     onClickOnCalendar,
     onClickOnChat,
-    isPage
+    cancelDeletePage,
+    cancelLeave,
+    proceedDeletePage,
+    proceedLeave,
+    isDeletePageOpen,
+    isLeaveOpen,
+    isPage,
+    isMember
 }: Props) {
 
     return (
@@ -123,15 +143,54 @@ export default function ProfileHeader({
                                     icon={followIcon}
                                 />
                             )
-                        )}
+                        )
+                    }
                     <SecondaryButton
                         text="Ver Calendario"
                         type="button"
                         enabled={true}
                         onClick={onClickOnCalendar}
                     />
+                    {isPage && isMember && (
+
+                        ownProfile ? (
+                            <DestructiveButton
+                                text="Eliminar pagina"
+                                type="button"
+                                onClick={onClickOnDeletePage}
+                            />
+
+                        ) : (
+                            <DestructiveButton
+                                text="Salir"
+                                type="button"
+                                onClick={onCLickOnLeavePage}
+                            />
+                        )
+                    )}
                 </div>
             </div>
+
+            {isDeletePageOpen && (
+                <Modal
+                    title={"¿Estas seguro de eliminar esta pagina?"}
+                    description="Esta acción no se puede deshacer"
+                    cancelText="Cancelar"
+                    deleteText="Eliminar"
+                    onCancel={cancelDeletePage}
+                    onProceed={proceedDeletePage}
+                />
+            )}
+            {isLeaveOpen && (
+                <Modal
+                    title={"¿Estas seguro de salir de esta pagina?"}
+                    description="Esta acción no se puede deshacer"
+                    cancelText="Quedarse"
+                    deleteText="Salir"
+                    onCancel={cancelLeave}
+                    onProceed={proceedLeave}
+                />
+            )}
         </div>
     )
 }
