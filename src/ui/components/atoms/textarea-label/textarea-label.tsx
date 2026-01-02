a15718b0d9888eeee1c8693d23c2269bd7740653
milagrso <milagrosberoncapra1@gmail.com>
2026-01-02 17:57:48 -0300

    Add: max input

--- src/ui/components/atoms/textarea-label/textarea-label.tsx
+++ src/ui/components/atoms/textarea-label/textarea-label.tsx
@@ -7,9 +7,11 @@ type Props = {
     placeholder: string;
     required: boolean;
     value?: string | undefined;
+    max?: number;
+    min?: number;
 }
 
-export default function TextAreaLabel({label, placeholder, id, required, value}: Props){
+export default function TextAreaLabel({label, placeholder, id, required, value, max, min}: Props){
     const [self, setSelf] = useState<string | undefined>(value || "")
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,7 +21,7 @@ export default function TextAreaLabel({label, placeholder, id, required, value}:
     return(
         <div className={style.container}>
             <label htmlFor={id}>{label}</label>
-		    <textarea placeholder={placeholder} name={id} id={id} value={self} onChange={handleChange} required={required} />
+		    <textarea placeholder={placeholder} name={id} id={id} value={self} onChange={handleChange} required={required} maxLength={max} minLength={min} />
         </div>
     )
 }
