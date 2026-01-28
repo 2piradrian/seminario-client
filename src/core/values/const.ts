e34ad923ad5c5c990f732712c42b8c69d952fd49
JulianTs0 <juliantorresssr@gmail.com>
2026-01-28 20:29:46 -0300

    add: const values

--- src/core/values/const.ts
+++ src/core/values/const.ts
@@ -1,3 +1,32 @@
 export const CONSTANTS = {
-    SELECT_OPTION_VALUE: "Seleccionar"
+    SELECT_OPTION_VALUE: "Seleccionar",
+    LOADING_SESSION: 'Iniciando sesión...',
+    SUCCESS_SESSION: '¡Sesión iniciada correctamente!',
+    
+    LOADING_REGISTER: 'Registrando usuario...',
+    SUCCESS_REGISTER: '¡Usuario registrado correctamente!',
+
+    LOADING_NEW_EVENT: 'Creando evento...',
+    SUCCESS_NEW_EVENT: '¡Evento creado correctamente!',
+
+    LOADING_NEW_PAGE: 'Creando página...',
+    SUCCESS_NEW_PAGE: '¡Página creada correctamente!',
+
+    LOADING_NEW_POST: 'Publicando post...',
+    SUCCESS_NEW_POST: '¡Post publicado correctamente!',
+
+    LOADING_EDIT_EVENT: 'Actualizando evento...',
+    SUCCESS_EDIT_EVENT: '¡Evento actualizado correctamente!',
+
+    LOADING_EDIT_PAGE: 'Actualizando página...',
+    SUCCESS_EDIT_PAGE: '¡Página actualizada correctamente!',
+
+    LOADING_EDIT_PASSWORD: 'Cambiando contraseña...',
+    SUCCESS_EDIT_PASSWORD: '¡Contraseña cambiada correctamente!',
+
+    LOADING_EDIT_POST: 'Actualizando post...',
+    SUCCESS_EDIT_POST: '¡Post actualizado correctamente!',
+
+    LOADING_EDIT_PROFILE: 'Actualizando perfil...',
+    SUCCESS_EDIT_PROFILE: '¡Perfil actualizado correctamente!',
 }
